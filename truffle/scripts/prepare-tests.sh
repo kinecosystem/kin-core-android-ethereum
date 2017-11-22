8b4e81a3b0a11450a4fda9ed91f99bbec601c9cb
Oren Zakay <oren.zakay@kik.com>
2017-11-22 16:06:59 +0200

    fix travis truffle tstrpc

--- truffle/scripts/prepare-tests.sh
+++ truffle/scripts/prepare-tests.sh
@@ -9,7 +9,7 @@ configFile="./kin-sdk-core/src/androidTest/assets/testConfig.json"
 
 # export token contract address environment variable
 export TOKEN_CONTRACT_ADDRESS=$(cat ./truffle/token-contract-address)
-
+test -n "${TOKEN_CONTRACT_ADDRESS}"
 echo "Set Contract Address ${TOKEN_CONTRACT_ADDRESS}"
 echo ""
 # write contract address to testConfig.json, will be read by androidTest
